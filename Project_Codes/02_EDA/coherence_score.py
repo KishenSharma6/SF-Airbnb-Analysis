6abc57ed1547bee431b84c6bf8173e8474d4cdfb
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-06-17 14:54:03 -0700

    First commit

---
+++ Project_Codes/02_EDA/coherence_score.py
@@ -0,0 +1,25 @@
+def coherence_value_calculator(dictionary, corpus, texts, limit, start=2, step=3, coherence = 'c_v'):
+    """
+    Compute coherence score for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        model=LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics)
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence=coherence)
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
